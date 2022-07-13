5d5644cc536af15cea035ac01bac965f03721643
martina-nu <martinanu98@gmail.com>
2022-07-13 14:14:52 +0000

    adding solution

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Template for Machine Learning projects
+# Logistic Regression Project
+
+## Steps:
+
+- Understand your data
+- Follow the cleaning instructions
+- Model your data using Logistic Regression
+- Optimize your model and save it.
+
--- src/app.py
+++ src/app.py
@@ -1,4 +1,34 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+from sklearn import model_selection
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import pickle
 
-# your code here
+df = pd.read_csv("data/processed/clean_bank.csv")
+
+X = df.drop('y', axis=1)
+y = df['y']
+
+columns = X.columns
+scaler = StandardScaler()
+X_std = scaler.fit_transform(X)
+X_std = pd.DataFrame(X_std, columns = columns)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X_std, y, test_size= 0.2, random_state=0)
+
+
+model = LogisticRegression(C= 0.1, penalty='l2', solver= 'liblinear')
+model.fit(X_train, y_train)
+
+filename = 'models/model.sav'
+
+pickle.dump(model, open(filename,'wb'))
+
+#load model
+
+loaded_model = pickle.load(open(filename,'rb'))
+
+result = loaded_model.score(X_test, y_test)
+
+print(result)
+
